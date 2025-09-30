3ab8d12f40314451ade547d019d3713a8e4d2cec
imaihayato2025 <imaihayato2024@gmail.com>
2025-09-30 11:57:17 +0900

    aaa

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacttだよ
+          Learn Reacttだよお
         </a>
       </header>
     </div>
